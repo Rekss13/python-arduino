dc05e0795c763493c2e65088a232cb79b04f7e57
Roman Plotnikov <rekss13@gmail.com>
2023-02-21 11:44:03 +0300

    send-receive messages via uart

--- sketch_serial.ino
+++ sketch_serial.ino
@@ -2,6 +2,9 @@ void setup() {
   Serial.begin(9600);
 }
 void loop() {
-  Serial.println("Hello from Arduino!");
-  delay(1000);
+  if (Serial.available() > 0) {
+    String data = Serial.readStringUntil('\n');
+    Serial.print("You sent me: ");
+    Serial.println(data);
+  }
 }
