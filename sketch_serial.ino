88d2f7efaa3276cebf26e1b90cf2dd63642e1fb0
Roman Plotnikov <rekss13@gmail.com>
2023-02-21 12:19:50 +0300

    power control test

--- sketch_serial.ino
+++ sketch_serial.ino
@@ -1,16 +1,59 @@
-#define PIN_10 10
-#define PIN_11 11
+#define PIN_13 13
+const unsigned long SHORT_LAG = 20000;
+const unsigned long LONG_LAG = 60000;
+unsigned long previousMillis = 0;
+unsigned long previousMillisError = 0;
+unsigned long lag;
+unsigned long lagError;
+long delta;
+bool isStop;
+
 void setup() {
-  digitalWrite(PIN_10, OUTPUT);
-  digitalWrite(PIN_10, OUTPUT);
+  digitalWrite(PIN_13, OUTPUT);
   Serial.begin(9600);
-  digitalWrite(PIN_10, LOW);
-  digitalWrite(PIN_11, HIGH);  
+  setStart();  
 }
+
+void setStart() {
+  lag = LONG_LAG;
+  lagError = LONG_LAG * 2;
+  previousMillis = millis();
+  previousMillisError = millis();
+  digitalWrite(PIN_13, LOW);
+  isStop = false;
+}
+
+void setError() {
+  digitalWrite(PIN_13, HIGH);
+  delay(10000);
+  setStart();
+}
+
 void loop() {
+  unsigned long currentMillis = millis();
+  if (!isStop && currentMillis - previousMillis >= lag) {   
+    previousMillis = currentMillis;
+    Serial.println("getState");
+  }
+  if (!isStop && currentMillis - previousMillisError >= lagError) {   
+    previousMillisError = currentMillis;
+    Serial.println("error");
+    setError();
+  }
   if (Serial.available() > 0) {
     String data = Serial.readStringUntil('\n');
-    Serial.print("You sent me: ");
+    if (data == "start" || data == "work") {
+      lag = SHORT_LAG;
+      lagError = LONG_LAG;
+      previousMillisError = currentMillis;
+    } else if (data == "restart") {
+      setStart();
+    } else if (data == "stop") {
+      isStop = true;
+    } else if (data == "error") {
+      setError();
+    }
+    Serial.print("You send me: ");
     Serial.println(data);
-  }
+  } 
 }
